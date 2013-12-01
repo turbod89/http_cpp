3903fa5a0adf22aa57af160d9539ec78083f091e
Daniel Torres <dani10sa2@hotmail.com>
2013-12-01 16:57:10 +0100

    adding classes

---
+++ classes/json.h
@@ -0,0 +1,29 @@
+/*
+ *  Class: json
+ *  Author: Daniel Torres
+ *  Filename: json.h
+ * 
+ * 
+ * 
+ *  Notes: 
+ * 
+ *
+ */
+
+#ifndef H_JSON
+#define H_JSON
+
+#include<iostream>
+
+class json {
+
+  private:
+    
+  
+  
+  public:
+
+}
+
+
+#endif
